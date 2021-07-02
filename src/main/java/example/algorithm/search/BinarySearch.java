269fc1329742fb325e4d333a89cd51a6f3e2ce2f
liurui <liurui@greatld.com>
2021-07-02 18:03:17 +0800

    加入算法相关包

---
+++ src/main/java/example/algorithm/search/BinarySearch.java
@@ -0,0 +1,71 @@
+package example.algorithm.search;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+/**
+ * 实现一个插入数据自动排序的List；
+ * 核心实现：插入自动排序，说明该list是排好序的，需要定位具体的插入位置；
+ * 对于一个元素，list中已经存在相同排序顺序的元素。应该排在这些元素的后面
+ *
+ * @author LR<br>
+ * @version 1.0 <br>
+ * @date 2021/7/2 17:55
+ */
+public class BinarySearch {
+
+	// Collections.sort
+	// 如果排序比较相等，会插入到相同顺序的前面，不符合
+	public static <E> int binarySearchIndex(List<E> list, E e, Comparator<E> comparator) {
+		int low = 0;
+		int high = list.size() - 1;
+		while (low <= high) {
+			int mid = (low + high) >>> 1;
+
+			int cmp = comparator.compare(list.get(mid), e);
+
+			if (cmp < 0)
+				low = mid + 1;
+			else if (cmp > 0)
+				high = mid - 1;
+			else
+				return mid;  // 相等，应该排在后面
+		}
+		return low;
+
+	}
+
+	// joda -> SortedArrayList
+	protected static <E> int findInsertionPoint(List<E> list, E e, Comparator<E> comparator) {
+		int low = 0;
+		int high = list.size() - 1;
+		while (low <= high) {
+			int mid = (low + high) >>> 1;
+			int delta = comparator.compare(list.get(mid), e);
+
+			if (delta > 0) {
+				high = mid - 1;
+			} else {
+				low = mid + 1;
+			}
+		}
+
+		return low;
+	}
+
+	public static void main(String[] args) {
+		List<Integer> list = IntStream.of(1, 2, 4, 4, 5, 6).boxed().collect(Collectors.toList());
+		// list中已经有元素 4 了，在插入 4，找出插入的下标
+//		System.out.println(binarySearchIndex(list, 1, Comparator.naturalOrder()));
+//		System.out.println(binarySearchIndex(list, 2, Comparator.naturalOrder()));
+//		System.out.println(binarySearchIndex(list, 3, Comparator.naturalOrder()));
+		System.out.println(binarySearchIndex(list, 4, Comparator.naturalOrder()));
+		System.out.println(findInsertionPoint(list, 4, Comparator.naturalOrder()));
+//		System.out.println(binarySearchIndex(list, 5, Comparator.naturalOrder()));
+//		System.out.println(binarySearchIndex(list, 6, Comparator.naturalOrder()));
+//		System.out.println(binarySearchIndex(list, 7, Comparator.naturalOrder()));
+	}
+
+}
---
+++ src/main/java/example/demo/code/Enums.java
@@ -0,0 +1,95 @@
+/**
+ * 版权申明: 苏州朗动科技有限公司<br>
+ * 项目描述: 企查查云聚接口平台
+ */
+
+package example.demo.code;
+
+
+/**
+ * 描述:TOTO 请补全模块<br>
+ *
+ * @author LR<br>
+ * @version 1.0 <br>
+ * @date 2021/7/2 15:58
+ */
+public enum Enums {
+
+	ONE(1,"ONE", withType(Type.SYSTEM, Type.EMAIL, Type.PHONE)),
+
+	TWO(2,"TWO", withType(Type.SYSTEM)),
+
+	THREE(3,"THREE", withType(Type.SYSTEM, Type.EMAIL)),
+
+	FOUR(4,"FOUR", withType(Type.SYSTEM)),
+
+	FIVE(5,"FIVE", withType(Type.SYSTEM, Type.EMAIL, Type.PHONE)),
+
+	SIX(6,"SIX", withType(Type.SYSTEM, Type.EMAIL, Type.PHONE));// 企账户
+
+	enum Type {
+		SYSTEM(1),
+		EMAIL(2),
+		PHONE(3);
+
+		static final int TOTAL = Type.values().length;
+		final int position;
+		Type(int position) {
+			this.position = position;
+		}
+		private int mask() {
+			return 1 << (TOTAL - position);
+		}
+	}
+
+	private static int withType(Type... types) {
+		int mask = 0;
+		for (Type type : types) {
+			mask |= type.mask();
+		}
+		return mask;
+	}
+
+	private final int type;
+	private final String des;
+	private final int initParam;
+
+
+	Enums(int type, String des, int initParam) {
+		this.type = type;
+		this.des = des;
+		this.initParam = initParam;
+	}
+
+	public int getType() {
+		return type;
+	}
+
+	public String getDes() {
+		return des;
+	}
+
+	public boolean withSystem() {
+		return (initParam & Type.SYSTEM.mask()) != 0;
+	}
+
+	public boolean withEmail() {
+		return (initParam & Type.EMAIL.mask()) != 0;
+	}
+
+	public boolean withPhone() {
+		return (initParam & Type.EMAIL.mask()) != 0;
+	}
+
+	private static final Enums[] ENUMS = Enums.values();
+
+	public static Enums find(int type) {
+		if (type < 1 || type > 6) {
+			throw new RuntimeException("类型错误");
+		}
+		return ENUMS[type - 1];
+	}
+
+
+}
+
